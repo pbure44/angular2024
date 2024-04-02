f5ea5682a205b1878aaf6ad22d76b5b88696860f
pbure44 <pbure.ua@gmail.com>
2024-04-02 12:08:15 +0300

    initial commit

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {IUser} from "../../interfaces/user.interface";
+import {UserComponent} from "../user/user.component";
+import {NgFor, NgIf} from "@angular/common";
+import {UserDetailsComponent} from "../user-details/user-details.component";
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [
+    UserComponent,
+    NgFor,
+    UserDetailsComponent,
+    NgIf
+  ],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent implements OnInit {
+  users: IUser[];
+  usersDetails: IUser;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe(value => this.users = value);
+  }
+
+  getUserEvent(user: IUser) {
+    this.usersDetails = user;
+  }
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../interfaces/user.interface";
+import {urls} from "../constants/urls";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+  getAll():Observable<IUser[]>{
+    return this.httpClient.get<IUser[]>(urls.users.base)
+  }
 }
